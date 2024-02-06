e6b907f0ecc9429458500d8af4ffe1b57a806147
saikumar <saim.kumar27@gmail.com>
2024-02-06 20:31:49 +0530

    Enhanced UI

--- app/src/main/java/com/sample/mycategory/presentation/category_list/CategoryItem.kt
+++ app/src/main/java/com/sample/mycategory/presentation/category_list/CategoryItem.kt
@@ -1,24 +1,43 @@
 package com.sample.mycategory.presentation.category_list
 
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.MaterialTheme.typography
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.contentColorFor
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.sample.domain.model.Category
 
 @Composable
 fun CategoryItem(category: Category) {
-    // Your Compose UI components for each category item go here
-    Text(
-        text = category.categoryName,
-        modifier = Modifier.padding(8.dp),
-        style = TextStyle(
-            fontWeight = FontWeight.Bold,
-            fontSize = 20.sp
-        )
-    )
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        shape = RoundedCornerShape(8.dp),
+        backgroundColor = MaterialTheme.colors.background, // Use a relevant property
+        elevation = 4.dp
+    ) {
+        Surface(
+            modifier = Modifier.fillMaxWidth(),
+            color = Color.Transparent
+        ) {
+            Text(
+                text = category.categoryName,
+                modifier = Modifier.padding(16.dp),
+                style = typography.h6.copy(
+                    fontWeight = FontWeight.Bold,
+                    color = contentColorFor(MaterialTheme.colors.background) // Use a relevant property
+                )
+            )
+        }
+    }
 }
